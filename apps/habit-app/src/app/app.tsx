b75a174cc94e0d2bdfe251be54ae81eea6fab561
Su Anli <su.anli@anlitz.com>
2022-02-13 14:56:50 +0800

    feat(habit-app): add BottomTabBar, HomeTabs

--- apps/habit-app/src/app/app.tsx
+++ apps/habit-app/src/app/app.tsx
@@ -1,7 +1,19 @@
 import { Screen, Text, View } from '@nx-react-native/shared/ui'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
+import Icon from 'react-native-vector-icons/dist/MaterialCommunityIcons'
+
+const TabBarIcon = ({
+  name,
+  color,
+  size
+}: {
+  name: string
+  color: string
+  size: number
+}): JSX.Element => <Icon name={name} size={size} color={color} />
 
 const HomeScreen = (): JSX.Element => {
   return (
@@ -15,10 +27,32 @@ const HomeScreen = (): JSX.Element => {
 
 const Stack = createNativeStackNavigator()
 
+const Tab = createBottomTabNavigator()
+
+export const HomeTabs = (): JSX.Element => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <TabBarIcon name="home-variant" color={color} size={size} />
+          )
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
+
 export const App = (): JSX.Element => (
   <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen
+        name="HomeTabs"
+        component={HomeTabs}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   </NavigationContainer>
 )
