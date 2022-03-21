13743b66c1f36123bc1a458518ce3d46a3c5da27
Su Anli <anli@users.noreply.github.com>
2022-03-21 13:53:59 +0800

    chore(app): add sentry (#52)
    closes #44

--- apps/habit-app/src/main.tsx
+++ apps/habit-app/src/main.tsx
@@ -1,6 +1,18 @@
 import './wdyr'
 
-import { AppRegistry } from 'react-native'
+import * as Sentry from '@sentry/react-native'
+import { AppRegistry, LogBox } from 'react-native'
 import { App } from './app'
+import Config from 'react-native-config'
 
-AppRegistry.registerComponent('main', () => App)
+LogBox.ignoreLogs([
+  'Require cycle: node_modules/react-native/Libraries/Network/fetch.js -> node_modules/whatwg-fetch/dist/fetch.umd.js -> node_modules/react-native/Libraries/Network/fetch.js'
+])
+
+Sentry.init({
+  dsn: Config.SENTRY_DSN,
+  tracesSampleRate: __DEV__ ? 1.0 : 0.2,
+  environment: __DEV__ ? 'develop' : 'production'
+})
+
+AppRegistry.registerComponent('main', () => Sentry.wrap(App))
