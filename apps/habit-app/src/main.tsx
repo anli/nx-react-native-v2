58c53ba3e02cd3cf0f5f04fa5301d988199bb9bb
Su Anli <anli@users.noreply.github.com>
2022-03-27 20:20:11 +0800

    chore(app): add production env (#60)

--- apps/habit-app/src/main.tsx
+++ apps/habit-app/src/main.tsx
@@ -11,16 +11,16 @@ LogBox.ignoreLogs([
   "EventEmitter.removeListener('appStateDidChange', ...): Method has been deprecated" // split library
 ])
 
-if (!__DEV__ || (__DEV__ && Config.SENTRY_INIT_IN_DEV === 'true')) {
+if (Config.SENTRY_INIT === 'true') {
   Sentry.init({
     dsn: Config.SENTRY_DSN,
-    tracesSampleRate: __DEV__ ? 1.0 : 0.2,
-    environment: __DEV__ ? 'develop' : 'production'
+    tracesSampleRate: 1.0,
+    environment: Config.SENTRY_ENVIRONMENT
   })
 }
 
-if (!__DEV__ || (__DEV__ && Config.HEAP_INIT_IN_DEV === 'true')) {
-  Heap.setAppId(__DEV__ ? Config.HEAP_APP_ID_DEV : Config.HEAP_APP_ID_PROD)
+if (Config.HEAP_INIT === 'true') {
+  Heap.setAppId(Config.HEAP_APP_ID)
 }
 
 AppRegistry.registerComponent('main', () => Sentry.wrap(App))
